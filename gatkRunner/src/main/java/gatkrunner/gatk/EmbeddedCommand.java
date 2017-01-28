1e53deb9e0c423592855629784f8383f277939b7
Leonardo Varuzza <varuzza@gmail.com>
2017-01-28 10:43:39 -0200

    update package name

---
+++ gatkRunner/src/main/java/gatkrunner/gatk/EmbeddedCommand.java
@@ -0,0 +1,33 @@
+package com.tmo.gatk;
+
+import org.broadinstitute.gatk.engine.CommandLineExecutable;
+import org.broadinstitute.gatk.engine.arguments.GATKArgumentCollection;
+import org.broadinstitute.gatk.utils.commandline.Argument;
+import org.broadinstitute.gatk.utils.commandline.ArgumentCollection;
+import org.broadinstitute.gatk.utils.progressmeter.ProgressMeter;
+
+public class EmbeddedCommand extends CommandLineExecutable {
+
+	@Argument(fullName = "analysis_type", shortName = "T", doc = "Name of the tool to run")
+	private String analysisName = null;
+    
+    @ArgumentCollection
+	private GATKArgumentCollection args = new GATKArgumentCollection();
+		
+	@Override
+	public String getAnalysisName() {
+		
+		return analysisName;
+	}
+
+	@Override
+	protected GATKArgumentCollection getArgumentCollection() {
+		return args;
+	}
+
+	
+	public ProgressMeter getProgressMeter() {
+		return engine.getProgressMeter();
+	}
+	
+}
