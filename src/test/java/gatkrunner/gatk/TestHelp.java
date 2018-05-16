417ba6167995c2d7ffafbd22833975fd7b55dfce
Leonardo Varuza <varuzza@gmail.com>
2018-05-16 08:56:49 -0300

    Change testhelp

--- src/test/java/gatkrunner/gatk/TestHelp.java
+++ src/test/java/gatkrunner/gatk/TestHelp.java
@@ -1,6 +1,14 @@
 package gatkrunner.gatk;
 
+import org.broadinstitute.gatk.engine.walkers.Walker;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 import org.testng.annotations.Test;
+
+import java.util.Set;
 
 public class TestHelp {
 
@@ -10,11 +18,21 @@ public class TestHelp {
     public void showHelp() {
         //gatk.showHelp();
 
+        Reflections reflections = new Reflections((new ConfigurationBuilder()).setUrls(ClasspathHelper.forPackage("org.broadinstitute.gatk")).setScanners(new Scanner[]{new SubTypesScanner()}));
+
+        Set<Class<? extends Walker>> allTypes = reflections.getSubTypesOf(Walker.class);
+
+        for(Class<? extends Walker> klass:allTypes) {
+            System.out.println(klass.getName());
+        }
+
+
         EmbeddedCommand instance = new EmbeddedCommand();
         try  {
             EmbeddedCommand.start(instance, new String[]{"-T","CombineVariants"});
         } catch(Exception e) {
             e.printStackTrace();
         }
+
     }
 }
